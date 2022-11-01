becbaf047ee11687c20d815aec2ba9d234b6a868
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-11-01 10:31:21 +0300

    enumarables

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,33 @@
+require 'pry'
+
 # Write your code here
+# def reverse_each_word(sentence)
+#  words = sentence.split 
+#  reversed_words = []
+#  words.each do |word|
+#    reversed_words << word.reverse
+#  end 
+#  reversed_words.join("")
+# end
+
+# binding.pry
+
+# 0
+# puts reverse_each_word("Hello there,and how are you?")
+
+
+
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = words.map do |word|
+        word.reverse
+    end
+    reversed_words.join("")
+end
+puts reverse_each_word("Hello there,and how are you?")
+
+
+def reverse_each_word(sentence)
+    sentence.split.map(&:reverse).join(" ")
+  end
